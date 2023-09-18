6fca53ac08ff226aa2a6a8e8a424a9335a92b223
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-18 23:14:06 +0700

    Method insert()

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -7,8 +7,20 @@ use App\Models\Mahasiswa;
 
 class MahasiswaController extends Controller
 {
-    public function cekObject() {
+    public function cekObject()
+    {
         $mahasiswa = new Mahasiswa;
+        dump($mahasiswa);
+    }
+
+    public function insert()
+    {
+        $mahasiswa = new Mahasiswa;
+        $mahasiswa->nim = '19003036';
+        $mahasiswa->nama = 'Sari Citra Lestari';
+        $mahasiswa->tanggal_lahir = '2001-12-31';
+        $mahasiswa->ipk = 3.5;
+        $mahasiswa->save();
         dump($mahasiswa);
     }
 }
