6b809b320cba0e3367a6e327d87af7a41441a7b5
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-18 23:06:23 +0700

    Create model Mahasiswa
    Membuat method cekObject()

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function cekObject() {
+        $mahasiswa = new Mahasiswa;
+        dump($mahasiswa);
+    }
 }
